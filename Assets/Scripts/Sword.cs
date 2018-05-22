b14a048fc0651a30860cf22a9df6f08cb50bea9b
Jack Cooper <pog777six@gmail.com>
2018-05-22 23:31:02 +1000

    Removed Bolt and slash attack
    Removed the addon bolt - it was causing the game to take too long to load and wasn't being used.
    
    Added the Slashing animation to Rmb.
    
    Fixed some animations

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+    public bool attacking = false;
+
+    public BoxCollider2D attackTrigger;
+
+    private float attackTimer = 0;
+    private float attackCd = 0.3f;
+
+    private PlayerControl playerCtrl;       // Reference to the PlayerControl script.
+    private Animator anim;                  // Reference to the Animator component.
+
+    void Awake()
+    {
+        // Setting up the references.
+        anim = transform.root.gameObject.GetComponent<Animator>();
+        playerCtrl = transform.root.GetComponent<PlayerControl>();
+        attackTrigger.enabled = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        // If the RMB button is pressed...
+        if (Input.GetButtonDown("Fire2") && playerCtrl.crouching == false && !attacking)
+        {
+            // ... set the animator Shoot trigger parameter and play the audioclip.
+            anim.SetTrigger("Slash");
+            //GetComponent<AudioSource>().Play();
+
+            attacking = true;
+            attackTimer = attackCd;
+
+            attackTrigger.enabled = true;      
+            
+        }
+
+        if (attacking)
+        {
+            if(attackTimer > 0)
+            {
+                attackTimer -= Time.deltaTime;
+            }
+            else
+            {
+                attacking = false;
+                attackTrigger.enabled = false;
+            }
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //Physics.IgnoreCollision(explosion.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+    }
+}
