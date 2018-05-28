04c8d1163db8ed00ab12fff2414a390b6032d32a
Jack Cooper <pog777six@gmail.com>
2018-05-29 00:40:53 +1000

    Added collectables and scoring system
    tweaked other stuff too

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public float rotationDegreesPerSecond = 45f;
+    public float rotationDegreesAmount = 360f;
+    private float totalRotation = 0;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if we haven't reached the desired rotation, swing
+
+        if (Mathf.Abs(totalRotation) < Mathf.Abs(rotationDegreesAmount))
+            SwingOpen();
+            totalRotation = 0;
+    }
+
+    void SwingOpen()
+    {
+        float currentAngle = transform.rotation.eulerAngles.y;
+        transform.rotation =
+         Quaternion.AngleAxis(currentAngle + (Time.deltaTime * rotationDegreesPerSecond), Vector3.up);
+        totalRotation += Time.deltaTime * rotationDegreesPerSecond;
+    }
+}
