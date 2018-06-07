09cf03798908fe757e1b4650f72546877fbf1510
Animator1 <39936619+Animator1@users.noreply.github.com>
2018-06-07 15:29:54 +1000

    Changes to Level 4

--- Assets/Scripts/BladeEnemy.cs
+++ Assets/Scripts/BladeEnemy.cs
@@ -6,22 +6,28 @@ public class BladeEnemy : MonoBehaviour {
 
 	public float range;
     public int HP = 1;
-	public GameObject Player;
+	public GameObject player;
 	public Rigidbody2D myRigid;
 
     // Use this for initialization
     void Start()
     {
-        Player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update () {
-		if (Vector3.Distance(Player.transform.position, this.transform.position) < range){
-			myRigid.velocity = new Vector2(Player.transform.position.x - this.transform.position.x , Player.transform.position.y - this.transform.position.y);
-		} else {
-			myRigid.velocity = new Vector2(0 , 0);
-		}
+        if (player)
+        {
+            if (Vector3.Distance(player.transform.position, this.transform.position) < range)
+            {
+                myRigid.velocity = new Vector2(player.transform.position.x - this.transform.position.x, player.transform.position.y - this.transform.position.y);
+            }
+            else
+            {
+                myRigid.velocity = new Vector2(0, 0);
+            }
+        }
     }
 
     private void FixedUpdate()
