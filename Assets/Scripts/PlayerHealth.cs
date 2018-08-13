4f58d1fa5ba17630069d4f3f705224053633e7e9
Jack Cooper <pog777six@gmail.com>
2018-08-13 14:26:41 +1000

    Last push

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -188,6 +188,8 @@ public class PlayerHealth : MonoBehaviour
             // ... Trigger the 'Die' animation state
             anim.SetTrigger("Die");
             anim.SetTrigger("Dead");
+        anim.SetBool("Climbing Move", false);
+        anim.SetBool("Climbing", false);
         deathCounter.AddDeath();
 
         StartCoroutine("Respawn");
@@ -204,8 +206,10 @@ public class PlayerHealth : MonoBehaviour
 
         health = check.health;
 
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(1.5f);
         anim.SetTrigger("Respawn");
+        anim.SetBool("Climbing Move", false);
+        anim.SetBool("Climbing", false);
         playerControl.SetPosition(check.checkPointPos);
         sceneName = SceneManager.GetActiveScene().name;
         if (sceneName.Equals("Level4"))
